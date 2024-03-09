547e95710021db778cd4c39829fe7051f3590337
quicklybly <quickly.tawer@gmail.com>
2024-03-09 17:53:10 +0300

    add lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void validate_size(int height, int width) {
+    if (height < 2) {
+        throw invalid_argument("Matrix height should be >= 2");
+    }
+    if (width < 1) {
+        throw invalid_argument("Matrix width should be >= 1");
+    }
+}
+
+int** init_array(int height, int width) {
+
+    int **arr = new int*[height];
+
+    for (int i = 0; i < height; ++i) {
+        arr[i] = new int[width];
+    }
+
+    return arr;
+}
+
+void fill_array_from_string(int* arr, const string& s, int width) {
+    if (s.size() != width) {
+        throw invalid_argument("String length should be equal to matrix width");
+    }
+    for (int i = 0; i < s.size(); ++i) {
+        int digit = s[i] - '0';
+        if (digit < 0 || digit > 9) {
+            throw invalid_argument(&"Expected number, but got " [ s[i]]);
+        }
+        arr[i] = digit;
+    }
+}
+
+void fill_matrix(int **arr, int height, int width) {
+    for (int i = 2; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            arr[i][j] = arr[i - 1][j] + arr[i - 2][j];
+        }
+    }
+}
+
+void print_result(int** arr, int height, int width) {
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+void clear(int** arr, int height, int width) {
+    for (int i = 0; i < height; ++i) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int height, width;
+    cin >> height >> width;
+
+    validate_size(height, width);
+
+    auto arr = init_array(height, width);
+
+    string str;
+    cin >> str;
+    fill_array_from_string(arr[0], str, width);
+    cin >> str;
+    fill_array_from_string(arr[1], str, width);
+
+    fill_matrix(arr, height, width);
+
+    print_result(arr, height, width);
+    clear(arr, height, width);
+
     return 0;
 }
